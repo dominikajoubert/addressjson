c6dbe4e27005ad67d62b8dca0c2f22bca73a5dcd
Dominika <dominika.wr@gmail.com>
2022-12-04 19:07:08 +0200

    AddressTestClass was not included in inital commit

--- src/test/java/com/investec/addressjson/AddressjsonApplicationTests.java
+++ src/test/java/com/investec/addressjson/AddressjsonApplicationTests.java
@@ -1,13 +1,52 @@
 package com.investec.addressjson;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.investec.addressjson.manager.AddressManager;
+import com.investec.addressjson.manager.AddressValidator;
+import com.investec.addressjson.pojo.Address;
+import com.investec.addressjson.util.AddressConstants;
 
 @SpringBootTest
-class AddressjsonApplicationTests {
+class AddressJsonApplicationTests {
+
+	@MockBean
+	private AddressJsonApplication addressJsonApplication;
+	@Autowired
+	AddressManager addressManager;
+	@Autowired
+	AddressValidator addressValidator;
 
 	@Test
-	void contextLoads() {
+	public void testReadingFile() {
+
+		String path = "./src/test/java/com/investec/addressjson";
+		String fileName = "addresses.json";
+
+		File file = new File(path, fileName);
+		List<Address> addressList = addressManager.processPrintingOfAddressFile(file.getPath(),
+				AddressConstants.PHYSICAL);
+
+		Address address = addressList.get(0);
+
+		assertEquals("City 1", address.getCityOrTown());
+		assertEquals("1234", address.getPostalCode());
+		assertNotEquals("Suburb 3", address.getSuburbOrDistrict());
+
+		assertTrue(addressList.get(0).getIsAddressValid());
+		assertFalse(addressList.get(1).getIsAddressValid());
+		assertFalse(addressList.get(2).getIsAddressValid());
 	}
 
 }
